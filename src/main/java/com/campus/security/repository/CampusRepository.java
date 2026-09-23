67202f1239c63b53b6fa9d9494a35d4daa6c38bb
Skosana Angel Busisiwe <angelskosana82@gmail.com>
2026-09-23 16:12:01 +0200

    Delete src directory

--- src/main/java/com/campus/security/repository/CampusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.campus.security.repository;
-
-import com.campus.security.model.Campus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampusRepository extends JpaRepository<Campus, Integer> {
-}
